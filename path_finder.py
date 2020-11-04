67cbaab98d70654f6b40367bd5c9710943b77bc0
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-11-04 10:32:04 +0600

    Worked on Functionality of Path Finder

--- path_finder.py
+++ path_finder.py
@@ -6,3 +6,11 @@ try:
     from tkinter import ttk
     from tkinter import messagebox
     import os
+except:
+    import install_requirements
+    import pygame
+    import sys
+    import math
+    from tkinter import *
+    from tkinter import ttk
+    from tkinter import messagebox
