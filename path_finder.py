43dff8df0448fd6468e09e7739d87751ed8bc060
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-11-05 12:00:46 +0600

    Worked on Functionality of Path Finder

--- path_finder.py
+++ path_finder.py
@@ -14,5 +14,19 @@ except:
     from tkinter import *
     from tkinter import ttk
     from tkinter import messagebox
+    import os
 
 screen = pygame.display.set_mode((800, 800))
+
+class spot:
+    def __init__(self, x, y):
+        self.i = x
+        self.j = y
+        self.f = 0
+        self.g = 0
+        self.h = 0
+        self.neighbors = []
+        self.previous = None
+        self.obs = False
+        self.closed = False
+        self.value = 1
